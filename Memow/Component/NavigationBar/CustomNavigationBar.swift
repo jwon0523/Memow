0c530cf0b445dd17d3131c95430abaf105bc08ed
JaewonLee <jwon0523@gmail.com>
2024-03-15 19:25:13 +0900

    Initial Commit

---
+++ Memow/Component/NavigationBar/CustomNavigationBar.swift
@@ -0,0 +1,18 @@
+//
+//  CustomNavigationBar.swift
+//  Memow
+//
+//  Created by jaewon Lee on 3/15/24.
+//
+
+import SwiftUI
+
+struct CustomNavigationBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomNavigationBar()
+}
---
+++ Memow/Component/NavigationBar/NavigationBtnType.swift
@@ -0,0 +1,15 @@
+//
+//  NavigationBarType.swift
+//  Memow
+//
+//  Created by jaewon Lee on 3/15/24.
+//
+
+import Foundation
+
+enum NavigationBtnType: String {
+    case close
+    case eidt = "편집"
+    case complete = "완료"
+    case create
+}
