7248f9b4aac2991f69c2c093981d90aff1666af8
JaewonLee <jwon0523@gmail.com>
2024-03-15 18:47:13 +0900

    Initial Commit

---
+++ Memow/MemowApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemowApp.swift
+//  Memow
+//
+//  Created by jaewon Lee on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct MemowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
