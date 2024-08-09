2c7b3e79af49daf43d7058f4bdb7647576cc6d60
jwon0523 <jwon010523@gmail.com>
2024-08-10 01:20:37 +0900

    Changing icon image done

--- Memow/Component/NavigationBar/NavigationBtnType.swift
+++ Memow/Component/NavigationBar/NavigationBtnType.swift
@@ -11,10 +11,6 @@ enum NavigationBtnType: String {
     // CustomNavigationBar에서 title 외 나머지는 이미지로 보여줌
     case notes = "Notes"
     case close = "Close"
-    // 추후 디자인 나오면 추가 예정
-    case hamburgerMenuIcon
-    case kebabMenuIcon = "KebabMenu"
-    case meetbalMenulIcon
     case leftBack = "LeftBack"
     case add = "Create"
     case complete = "완료"
@@ -22,6 +18,6 @@ enum NavigationBtnType: String {
     case home = "house"
     case select = "Select"
     case memow = "Memow"
-    case sideMenuIcon = "MenuBar"
+    case sideMenuIcon = "SideMenuBar"
     case delete = "Trash"
 }
